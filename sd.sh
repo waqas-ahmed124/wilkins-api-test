65ec06a34bf9155258f1c34b45c27ad6ec97e520
waqas-ahmed124 <154249794+waqas-ahmed124@users.noreply.github.com>
2024-01-22 11:51:11 +0500

    Create sd.sh
    new script

---
+++ sd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Replace these variables with your actual values
+SSH_KEY_PATH="/home/azureuser/.ssh/id_rsa"
+SSH_USERNAME="azureuser"
+VM_IP="172.203.185.112"
+
+# SSH command to connect to the VM
+ssh -i "$SSH_KEY_PATH" "$SSH_USERNAME@$VM_IP"
+echo "Script ran successfully!"
